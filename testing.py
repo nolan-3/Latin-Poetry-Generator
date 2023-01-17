a5ff35bd5fe88626c15add6e7acef3c4e7395e1e
Nolan McCloskey <nolamccl@haverford.org>
2023-01-16 21:36:41 -0500

    google translation API

---
+++ testing.py
@@ -0,0 +1,26 @@
+GOOGLE_APPLICATION_CREDENTIALS = "/auth.json" 
+
+def translate_text_with_model(target, text, model="nmt"):
+    """Translates text into the target language.
+
+    Make sure your project is allowlisted.
+
+    Target must be an ISO 639-1 language code.
+    See https://g.co/cloud/translate/v2/translate-reference#supported_languages
+    """
+    from google.cloud import translate_v2 as translate
+
+    translate_client = translate.Client()
+
+    if isinstance(text, bytes):
+        text = text.decode("utf-8")
+
+    # Text can also be a sequence of strings, in which case this method
+    # will return a sequence of results for each text.
+    result = translate_client.translate(text, target_language=target, model=model)
+
+    print(u"Text: {}".format(result["input"]))
+    print(u"Translation: {}".format(result["translatedText"]))
+    print(u"Detected source language: {}".format(result["detectedSourceLanguage"]))
+
+translate_text_with_model('en','comedit saxum')
