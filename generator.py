0fb369ee79537f5735eaf87150aa3c8357be4014
Nolan McCloskey <nolamccl@haverford.org>
2023-01-19 10:31:39 -0500

    working full line translations

--- generator.py
+++ generator.py
@@ -1,64 +1,77 @@
 from data import table
+from translations import translations
 import random
 import string
 
-#replaces letters not included in the lookup table: j, u, y  
-def checkLetter(letter):
+# replaces letters not included in the lookup table: j, u, y
 
-    if letter == 'j':
+
+def checkLetter(letter):
+    if letter.isalpha():
+        if letter == 'j':
             letter = 'i'
-    elif letter == 'u':
+        elif letter == 'u':
             letter = 'v'
-    elif letter == 'y':
+        elif letter == 'y':
             letter = 'z'
+    else:
+        letter = ''
     return letter
 
 
 def generate(message):
+    # stores which half line was used for each selection
+    chosenLetters = []
     message = message.lower().strip()
     poem = ''
     translation = ''
     endOfLine = False
-    #current index in the string (only counting letters)
+    # current index in the string (only counting letters)
     charNum = 0
-    #number of dictionaries in the lookup table
+    # number of dictionaries in the lookup table
     numOfDicts = len(table)
-    print(numOfDicts)
 
-    #eliminate letters not included in the table
+
+    # eliminate letters not included in the table
     tempString = ''
     for i in range(0, len(message)):
         tempString += checkLetter(message[i])
     message = tempString
 
+    #make sure the poem has a full last line
+    if(len(message) % 2 == 1):
+        char = random.choice(string.ascii_letters).lower()
+        char = checkLetter(char)
+        message += char
 
     for i in range(0, len(message)):
+        # add letters from message to chosenLetters
+        if(i % 2 == 0):
+            chosenLetters.append([message[i],message[i+1]])
         char = message[i]
-        #if message length > number of dictionaries, recycle them
+        # if message length > number of dictionaries, recycle them
         dictNum = charNum % numOfDicts
-        #isalpha() isn't necessary but its a nice tool to have
+        # isalpha() isn't necessary but its a nice tool to have
+        # UPDATE none of this should be necessasry, all handled in cleansing message
         if char.isalpha() and ord(char) >= 97 and ord(char) <= 122:
 
             halfLine = table[dictNum][char][0]
-            print(halfLine)
             halfEng = table[dictNum][char][1]
-            print(translation)
-            #print a new line if at the end of a line of latin
-            if(endOfLine):
+            # add a new line if at the end of a line of latin
+            if (endOfLine):
                 poem += halfLine + '\n'
-                translation += halfEng + '\n'
                 endOfLine = False
             else:
                 poem += halfLine + ' '
-                translation += halfEng + ' '
                 endOfLine = True
             charNum += 1
-    #make sure the final line is complete
-    if(endOfLine):
-        dictNum = charNum % numOfDicts
-        char = random.choice(string.ascii_letters).lower()
-        char = checkLetter(char)
-        poem += table[dictNum][char][0]
-        translation += table[dictNum][char][1]
 
-    return [poem,translation]
+    # translate poem
+    for i in range(0,len(chosenLetters)):
+        firstLetter = chosenLetters[i][0]
+        secondLetter = chosenLetters[i][1]
+        index = i % 8
+        line = translations[index][firstLetter][secondLetter]
+        translation += line + '\n'
+
+    return [poem, translation]
